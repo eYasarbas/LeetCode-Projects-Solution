76d9ba83ef1d92c38b0584229a7681fd922d5485
Enes Yaşarbaş <yasarbas583@gmail.com>
2023-08-01 19:55:40 +0300

    Sum of Two Number in c#

---
+++ C#/SumofTwoNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SumofTwoNumbers;
+class SumofTwoNumbers
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(SumTwo(12, 34));
+    }
+
+    private static string SumTwo(int a, int b)
+    {
+        return ("The sum of " + a + " and " + b + " is: "+ (a + b));
+    }
+}
