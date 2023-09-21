017eb9a77b47be9519d78db73f8573bd7e45bb6b
Enes Yaşarbaş <yasarbas583@gmail.com>
2023-09-21 16:46:54 +0300

    Remove Element

---
+++ C#/RemoveElement/RemoveElement/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RemoveElement;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int RemoveElement(int[] nums, int val)
+
+        {
+            int result = 0;
+            foreach (var item in nums)
+            {
+                if (item != val)
+                {
+                    nums[result] = item;
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
