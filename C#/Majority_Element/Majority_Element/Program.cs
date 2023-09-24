52a5732f4dae161fef46efde7d756258f001199f
Enes Yaşarbaş <yasarbas583@gmail.com>
2023-09-24 13:04:04 +0300

    MajorityElement problem is fixed

---
+++ C#/Majority_Element/Majority_Element/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Majority_Element;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int MajorityElement(int[] nums)
+        {
+            var againCount = new Dictionary<int, int>();
+
+            foreach (var item in nums)
+                if (againCount.ContainsKey(item))
+                    againCount[item]++;
+                else
+                    againCount[item] = 1;
+
+            var mostAgain = againCount.OrderByDescending(x => x.Value).First().Key;
+
+            return mostAgain;
+        }
+
+        int[] myList =
+        {
+            1, 2, 3, 4, 2, 2, 5, 6, 2, 7, 8, 9, 2
+        };
+
+        Console.WriteLine(MajorityElement(myList));
+    }
+}
